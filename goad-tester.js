95464bcc88a589d4c951be71a8a32be219e6275c
gazcn007-tableau <sliu@tableau.com>
2017-09-29 13:18:11 -0700

    update current tester code

--- goad-tester.js
+++ goad-tester.js
@@ -5,23 +5,71 @@ const fsPath = require('fs-path');
 const Promise = require('bluebird');
 const exec = Promise.promisify(require('child_process').exec);
 
+// Default configuration
+var config = {
+    numberOfRequests: 1000,
+    numberOfConcurrency: 15,
+    scenerioRatios:{
+        screenshot: 0.3,
+        requests: 0.3,
+        clicking: 0.3
+    },
+    numberOfPagesToTest: 10,
+    domainURL: "https://public-aws-poc.dev.tabint.net"
+}
+
+async function readConfig(){
+    if(process.argv[2]=='-c' || process.argv[2]=='--config'){
+        if(process.argv[3]&&process.argv[3].split('.').pop()=='json'){
+            console.log(process.argv[3]);
+            fs.readFile(process.argv[3],'utf8', (err,data)=>{
+                if(err) {throw err};
+                config = JSON.parse(data);
+                console.log(data);
+            })
+        }
+    }
+}
+
 async function batchProcess(){
     fs.readFile("./viz-urls.json", "utf8", async (err, fileData)=>{
         var { urls } = JSON.parse(fileData);
         var fullList = [].concat.apply([], urls).map(e=>{
-            return "https://public-aws-poc.dev.tabint.net"+e;
+            return config.domainURL+e;
         });
-        var indexJson = [];
-        var index =1;
+        var url_index_mapping = [];
+        var index = 1;
+        fsPath.mkdir('/goad-results', function(err){
+          console.log('created a folder "goad-results" to save test results');
+        });
         for ( url of fullList){
-            indexJson.push({index, url});
-            await exec ('goad -n 10000 -c 10 --json-output="./goad-results/goad-'+index+'.json" "'+url+'"');
-            // console.log('goad -n 10000 -c 10 --json-output="./goad-'+index+'.json" "'+url+'"');
-            index++;
+            if(Math.random()<0.1){
+                url_index_mapping.push({index, url});
+                await exec ('goad -n'+ config.numberOfRequests + ' -c'+ config.numberOfConcurrency+' --json-output="./goad-results/goad-'+index+'.json" "https://jlyd5biq74.execute-api.us-west-2.amazonaws.com/dev/screenshot?url='+url+'"');
+                if(index == config.numberOfPagesToTest) break;
+                index++;
+            }
         }
+        fsPath.writeFile('./goad-results/goad-results.json', 
+            JSON.stringify({url_index_mapping,
+                numberOfRequests: config.numberOfRequests,
+                concurrency: config.numberOfConcurrency,
+                datetime: getDate()
+            }),(err)=>{
+                if(err) throw err;
+            });
     });
-    await exec('touch test.delete');
     return; 
 }
-batchProcess();
+
+async function runTest(){
+    await readConfig();
+    await batchProcess();
+}
+
+var getDate = ()=>{
+    var time = new Date();
+    return (time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+}
+runTest();
 
--- viz-url-parser.js
+++ viz-url-parser.js
@@ -1,41 +1,62 @@
-const CDP = require('chrome-remote-interface');
 const fs = require('fs');
+const CDP = require('chrome-remote-interface');
+const ChromePool = require('chrome-pool');
+const fsPath = require('fs-path');
 
-CDP({
-    host: 'localhost',
-    port: 9222,
-}, async client => {
-    fs.readFile("./profile-urls.json", "utf8", async (err, fileData)=>{
-        var { links, domain } = JSON.parse(fileData);
-        const {Page, DOM, Input} = client;
-        let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
-
+async function process(urls){
+    const chromepoll = await ChromePool.new({
+        maxTab : 5,
+        port : 9222
+    });
+    let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+    var toRet = [];
+    for (url of urls){
+        const { tabId,protocol } = await chromepoll.require();
+        const { Page,Target,Network, DOM } = protocol;
         await Page.enable();
-        for (link of links){
-            console.log('Loading The URL:', domain+link)
-            await Page.navigate({ url: domain+link});
-            await Page.loadEventFired();
-            await wait(8000);
-            console.log('parsing The URL:', domain+link)
-            const rootElement = await DOM.getDocument();
-            const { root: { nodeId } } = rootElement;
-            const { nodeIds: linkIDs } = await DOM.querySelectorAll({
-                selector: "iframe",
-                nodeId,
+        await Page.navigate({url})
+        await Page.loadEventFired();
+        await wait(3000);
+        const rootElement = await DOM.getDocument();
+        const { root: { nodeId } } = rootElement;
+        const { nodeIds: linkIDs } = await DOM.querySelectorAll({
+            selector: "iframe",
+            nodeId,
+        });
+        const attributes = await Promise.all(linkIDs.map((ID) =>
+            DOM.getAttributes({ nodeId: ID })
+        ));
+        // Atrributes are returned in single array and item pairs
+        const url_parsed = attributes
+            .map(x => x.attributes)
+            .filter(x => x.includes("ng-src"))
+            .map((attrs) => {
+                const index = attrs.indexOf("ng-src");
+                return attrs[index + 1];
             });
-            const attributes = await Promise.all(linkIDs.map((ID) =>
-                DOM.getAttributes({ nodeId: ID })
-            ));
-            // Atrributes are returned in single array and item pairs
-            const urls = attributes
-                .map(x => x.attributes)
-                .filter(x => x.includes("ng-src"))
-                .map((attrs) => {
-                    const index = attrs.indexOf("ng-src");
-                    return attrs[index + 1];
-                });
-            console.log(urls);
+        toRet.push(url_parsed);
+        await chromepoll.release(tabId);
+    }
+    await chromepoll.destroyPoll();
+    return toRet;
+}
+
+
+async function batchProcess(){
+    fs.readFile("./profile-urls.json", "utf8", async (err, fileData)=>{
+        var { urls } = JSON.parse(fileData);
+        var fullList = [];
+        for (let i = 0; i<urls.length; i=i+5){
+            console.log('Batch Processing :', i,' to ',i+5);
+            var res = await process(urls.slice(i,i+5));
+            console.log("finished : ", res);
+            fullList.push(res);
         }
-        await client.close();
-    })
-});
+        fsPath.writeFile('./viz-urls.json', JSON.stringify({urls: fullList},null,2), function (err){
+            if(err) throw err;
+            console.log('Parsing finished; file created in the local directory!')
+            return;
+        })
+    });
+}
+batchProcess();
